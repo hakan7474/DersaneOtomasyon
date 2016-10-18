f640a8138d5a6f238755a4fe16f0fd5028edbd0e
emrullahdnc <emrullahdnc@gmail.com>
2016-10-18 19:43:22 +0300

    .

--- DersaneOtomasyon.Admin/Controllers/OdemeController.cs
+++ DersaneOtomasyon.Admin/Controllers/OdemeController.cs
@@ -55,21 +55,90 @@ namespace DersaneOtomasyon.Admin.Controllers
             return RedirectToAction("Index",new {id=id.Value });
         }
 
-        public ActionResult Details(int? id)
+        public ActionResult Details(int? ogrId, int? OdemeId)
         {
-            if (id == null)
+            if (ogrId == null && OdemeId== null)
             {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest); 
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
 
-            var getir = _odemeRepository.GetById(id.Value);
+            var ogr = _ogrenciRepository.GetById(ogrId.Value);
 
-            if (getir == null)
+            ViewBag.SelectedOgrenci = ogr;
+
+            var odeme = _odemeRepository.GetById(OdemeId.Value);
+
+            if (odeme == null)
             {
                 return HttpNotFound();
             }
 
-            return View(getir);
+            return View(odeme);
         }
+
+        // Hata var
+
+        public ActionResult Edit(int? odemeId, int? ogrId)
+        {
+            if (odemeId == null || ogrId == null)   /// odeme id null geliyor.
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var ogr = _ogrenciRepository.GetById(ogrId.Value);
+
+            ViewBag.SelectedOgrenci = ogr;
+
+            var odeme = _odemeRepository.GetById(odemeId.Value);
+            if (odeme== null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(odeme);
+                        
+        }
+        [HttpPost,ValidateAntiForgeryToken]
+        public ActionResult Edit(Odeme odeme)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(odeme);
+            }
+
+            _odemeRepository.Update(odeme);
+            _odemeRepository.Save();
+
+            return RedirectToAction("Index", new { id = odeme.OgrenciId });
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id==null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var odeme = _odemeRepository.GetById(id.Value);
+
+            if (odeme==null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(odeme);
+        }
+
+        [HttpPost,ValidateAntiForgeryToken,ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id,FormCollection collection)
+        { 
+
+            _odemeRepository.Delete(id);
+            _odemeRepository.Save();
+
+            return RedirectToAction("Index",new {id=collection["ogrenciId"]});
+        }
+
+
     }
 }
